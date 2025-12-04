c63b5ea8cffe9b8dc68b1a7237aff24aa5314f2e
matias <matiass@udel.edu>
2025-12-04 14:46:12 -0500

    Testing

--- pre-parallel/nnp.cu
+++ pre-parallel/nnp.cu
@@ -164,39 +164,25 @@ void train_model(MODEL* model){
                 // ---------- Loss ----------
                 float* d_label_n = d_train_label + n * CLASSES;
                 lossGPU<<<1,1>>>(d_label_n,d_outa,d_loss);
-                // ---------- Backprop ----------
-                delta3<<<CLASSESblocks,BLOCKSIZE>>>(d_label_n,d_outa,d_delta3);
-                delta2<<<H2blocks,BLOCKSIZE>>>(d_W3,d_delta3,d_h2a,d_delta2);
-                delta1<<<H1blocks,BLOCKSIZE>>>(d_W2,d_delta2,d_h1a,d_delta1);
+                // // ---------- Backprop ----------
+                // delta3<<<CLASSESblocks,BLOCKSIZE>>>(d_label_n,d_outa,d_delta3);
+                // delta2<<<H2blocks,BLOCKSIZE>>>(d_W3,d_delta3,d_h2a,d_delta2);
+                // delta1<<<H1blocks,BLOCKSIZE>>>(d_W2,d_delta2,d_h1a,d_delta1);
 
-                // // ---------- Update ----------
-                //Update W3 and bias 3
-                updateWeight<<<H2blocks,BLOCKSIZE>>>(CLASSES,d_W3,d_delta3,d_h2a,H2);
-                updateBias<<<CLASSESblocks,BLOCKSIZE>>>(d_b3,d_delta3,CLASSES);
+                // // // ---------- Update ----------
+                // //Update W3 and bias 3
+                // updateWeight<<<H2blocks,BLOCKSIZE>>>(CLASSES,d_W3,d_delta3,d_h2a,H2);
+                // updateBias<<<CLASSESblocks,BLOCKSIZE>>>(d_b3,d_delta3,CLASSES);
 
-                //Update W2 and bias 2
-                updateWeight<<<H1blocks,BLOCKSIZE>>>(H2,d_W2,d_delta2,d_h1a,H1);
-                updateBias<<<H2blocks,BLOCKSIZE>>>(d_b2,d_delta2,H2);
+                // //Update W2 and bias 2
+                // updateWeight<<<H1blocks,BLOCKSIZE>>>(H2,d_W2,d_delta2,d_h1a,H1);
+                // updateBias<<<H2blocks,BLOCKSIZE>>>(d_b2,d_delta2,H2);
 
-                //Update W1 and bias 1
-                float* d_sample_n = d_training_data + n * SIZE;
-                updateWeight<<<(SIZE+BLOCKSIZE-1)/BLOCKSIZE,BLOCKSIZE>>>(H1,d_W1,d_delta1,d_sample_n,SIZE);
-                updateBias<<<H1blocks,BLOCKSIZE>>>(d_b1,d_delta1,H1);
+                // //Update W1 and bias 1
+                // float* d_sample_n = d_training_data + n * SIZE;
+                // updateWeight<<<(SIZE+BLOCKSIZE-1)/BLOCKSIZE,BLOCKSIZE>>>(H1,d_W1,d_delta1,d_sample_n,SIZE);
+                // updateBias<<<H1blocks,BLOCKSIZE>>>(d_b1,d_delta1,H1);
 
-                // for (int j=0;j<H2;j++)
-                //     for (int k=0;k<CLASSES;k++)
-                //         W3[j*CLASSES+k]+=LR*delta3[k]*h2a[j];
-                // for (int k=0;k<CLASSES;k++) b3[k]+=LR*delta3[k];
-
-                // for (int j=0;j<H1;j++)
-                //     for (int k=0;k<H2;k++)
-                //         W2[j*H2+k]+=LR*delta2[k]*h1a[j];
-                // for (int k=0;k<H2;k++) b2[k]+=LR*delta2[k];
-
-                // for (int i=0;i<SIZE;i++)
-                //     for (int j=0;j<H1;j++)
-                //         W1[i*H1+j]+=LR*delta1[j]*train_data[n*SIZE+i];
-                // for (int j=0;j<H1;j++) b1[j]+=LR*delta1[j];
            
         }
         cudaMemcpy(&h_loss, d_loss, sizeof(float), cudaMemcpyDeviceToHost);
